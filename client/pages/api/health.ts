5f086ce9b62a5818d979042c82da3c65ce7a6a18
Ashwin Hegde <ashwin.hegde3@gmail.com>
2023-03-27 23:22:47 +0530

    fix(app): client health check api

---
+++ client/pages/api/health.ts
@@ -0,0 +1,13 @@
+import type { NextApiRequest, NextApiResponse } from "next"
+import constants from "@/app/constants"
+
+type healthResponse = {
+  acknowledge: boolean
+}
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<healthResponse>) {
+  if (req.method === 'GET') {
+    return res.status(constants.HTTP_STATUS_CODE.OK).send({ acknowledge: true })
+  }
+  return res.status(constants.HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR).send({ acknowledge: false }) 
+}
--- client/src/app/constants.ts
+++ client/src/app/constants.ts
@@ -1,5 +1,9 @@
 const constants = {
   DOWNSTREAM_ENDPOINT: 'http://localhost:8005/api/v1',
+  HTTP_STATUS_CODE: {
+    "OK": 200,
+    "INTERNAL_SERVER_ERROR": 500
+  }
 }
 
 export default constants
