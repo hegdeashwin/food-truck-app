4a87bef0decec29c427acfe051eff7954b07318d
Ashwin Hegde <ashwin.hegde3@gmail.com>
2023-03-27 23:23:26 +0530

    refactor(health): types

--- client/pages/api/health.ts
+++ client/pages/api/health.ts
@@ -1,13 +1,18 @@
-import type { NextApiRequest, NextApiResponse } from "next"
-import constants from "@/app/constants"
+import type { NextApiRequest, NextApiResponse } from 'next'
+import constants from '@/app/constants'
 
 type healthResponse = {
   acknowledge: boolean
 }
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<healthResponse>) {
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<healthResponse>
+) {
   if (req.method === 'GET') {
     return res.status(constants.HTTP_STATUS_CODE.OK).send({ acknowledge: true })
   }
-  return res.status(constants.HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR).send({ acknowledge: false }) 
+  return res
+    .status(constants.HTTP_STATUS_CODE.INTERNAL_SERVER_ERROR)
+    .send({ acknowledge: false })
 }
